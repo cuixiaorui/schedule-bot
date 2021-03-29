9b1ea7ce2d333c91c2f25f4f06fea9dbedf0392f
Seaborn Lee <seabornlee.cn@gmail.com>
2021-03-29 20:50:19 +0800

    feat: check is date in next 5 event dates - WIP

--- test/model/calendar.test.js
+++ test/model/calendar.test.js
@@ -1,9 +1,19 @@
 const Calender = require("../../src/model/calender");
 describe("Calender", () => {
   it("should check date is valid", () => {
-    const calender = new Calender();
+    const calender = new Calender("20210101");
     expect(calender.validate("6666")).toBe(false);
     expect(calender.validate("0011")).toBe(false);
-    expect(calender.validate("1231")).toBe(true);
+    expect(calender.validate("0101")).toBe(true);
+  });
+
+  it("should check date is in next 5 event date", () => {
+    const calender = new Calender("20210101");
+    expect(calender.validate("0101")).toBe(true);
+    expect(calender.validate("0103")).toBe(true);
+    expect(calender.validate("0105")).toBe(true);
+    expect(calender.validate("0107")).toBe(true);
+    expect(calender.validate("0109")).toBe(true);
+    expect(calender.validate("0111")).toBe(false);
   });
 });
